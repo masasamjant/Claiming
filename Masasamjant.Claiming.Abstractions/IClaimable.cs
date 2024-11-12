d397edbf4e3012dd1ed11e8bad5a6298561aac04
Matti Jantti <m_jantti@hotmail.com>
2024-11-12 18:15:02 +0200

    Claiming abstractions for applications that will use claiming framework

---
+++ Masasamjant.Claiming.Abstractions/IClaimable.cs
@@ -0,0 +1,15 @@
+﻿namespace Masasamjant.Claiming
+{
+    /// <summary>
+    /// Represents object that can be claimed for user.
+    /// </summary>
+    public interface IClaimable
+    {
+        /// <summary>
+        /// Gets unique identifier to identify object instance in claiming. Usually this is some key information like
+        /// primary key value in database.
+        /// </summary>
+        /// <returns>A unique identifier of the object instance.</returns>
+        string GetClaimInstanceIdentifier();
+    }
+}
