7d68bc203c3402b3022a086e58b7a63e7cc82a87
Matti Jantti <m_jantti@hotmail.com>
2024-12-16 18:46:39 +0200

    Add documentation

--- Masasamjant.Claiming.Abstractions/ClaimManagerInstance.cs
+++ Masasamjant.Claiming.Abstractions/ClaimManagerInstance.cs
@@ -1,8 +1,18 @@
 ﻿namespace Masasamjant.Claiming
 {
+    /// <summary>
+    /// Defines what kind of instance of claim manager is used.
+    /// </summary>
     public enum ClaimManagerInstance : int
     {
+        /// <summary>
+        /// Use singleton instance.
+        /// </summary>
         Singleton = 0,
+
+        /// <summary>
+        /// Use transient instance.
+        /// </summary>
         Transient = 1
     }
 }
